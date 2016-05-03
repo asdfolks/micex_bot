2d847cf63d180840a26aac8239d26139e298e75b
Aleksey Kutepov <aleksey.kutepov@revelsystems.com>
2016-05-03 10:53:07 -0700

    micex_bot-8 Add KRW for korean expats

--- micex_bot.py
+++ micex_bot.py
@@ -17,7 +17,8 @@ _API_PREFIX = 'https://api.telegram.org/bot'
 _API = _API_PREFIX + _TOKEN
 _WEB_HOOK = os.environ.get('WEBHOOK_HOSTNAME') + urllib.quote_plus(_TOKEN)
 _MICEX_USDRUB_URL = 'http://www.micex.ru/issrpc/marketdata/currency/selt/daily/preview/result.json?collection_id=173&board_group_id=13'
-
+_YAHOO_FINANCE_RUBKRW_URL = 'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.xchange%20WHERE%20pair%3D%22RUBKRW%22%20%7C%20truncate(count%3D1)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
+_YAHOO_FINANCE_USDKRW_URL = 'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.xchange%20WHERE%20pair%3D%22USDKRW%22%20%7C%20truncate(count%3D1)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
 
 def set_webhook():
     headers = {'Content-Type': 'application/json'}
@@ -27,11 +28,36 @@ def set_webhook():
     sys.stderr.write('Webhook status: {0}: {1}'.format(r.status_code, r.text))
 
 
+def send_yahoo_finance_krw_data(to, base):
+    if to is None:
+        sys.stderr.write('Cannot find chat_id\n')
+    if base == 'rub':
+        r = requests.get(_YAHOO_FINANCE_RUBKRW_URL)
+    elif base == 'usd':
+        r = requests.get(_YAHOO_FINANCE_USDKRW_URL)
+    else:
+        r = requests.get(_YAHOO_FINANCE_USDKRW_URL)
+    sys.stderr.write('Yahoo reply {0}: {1}\n'.format(r.status_code, r.text))
+    data = json.loads(r.text.decode('utf-8'))
+    reply = data.get('query', {}).get('results', {}).get('rate', {})
+    message = 'Инструмент: {Name}: {Rate},\n'\
+              'Спрос: {Ask}, Предложение: {Bid},\n'\
+              'Последнее обновление {Date} {Time},\n\n'.format(**reply)
+
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post(
+        _API + '/sendMessage',
+        data=json.dumps({'chat_id': to, 'text': message}),
+        headers=headers)
+    sys.stderr.write('Telegram sendMessage reply {0}: {1}'.format(
+        r.status_code, r.text))
+
+
 def send_micex_usdrub_data(to):
     if to is None:
         sys.stderr.write('Cannot find chat_id\n')
     r = requests.get(_MICEX_USDRUB_URL)
-    sys.stderr.write('MICEX reply {0}: {1}'.format(r.status_code, r.text))
+    sys.stderr.write('MICEX reply {0}: {1}\n'.format(r.status_code, r.text))
     data = json.loads(r.text.decode('utf-8'))
     message = ''
     message_template = 'Инструмент: {SHORTNAME}: {LAST},\n'\
@@ -68,21 +94,22 @@ def web_hook():
     sys.stderr.write('Webhook received: {0}\n'.format(data))
 
     command, _ = parse_command(data)
-    if command != 'usd':
-        return 'OK'
-
     chat_id = data.get('message', {}).get('chat', {}).get('id')
-    send_micex_usdrub_data(chat_id)
+    if command == 'usd':
+        send_micex_usdrub_data(chat_id)
+    elif command == 'rubkrw':
+        send_yahoo_finance_krw_data(chat_id, 'rub')
+    elif command == 'usdkrw':
+        send_yahoo_finance_krw_data(chat_id, 'usd')
     return 'OK'
-
 
 if __name__ == '__main__':
     if not _TOKEN:
-        sys.stderr.write('Telegram bot token not configured. Exiting.')
+        sys.stderr.write('Telegram bot token not configured. Exiting.\n')
         sys.exit(status=-1)
     set_webhook()
     port = int(os.environ.get("PORT", 80))
     try:
         app.run(host='0.0.0.0', port=port)
     except Exception as exc:
-        sys.stderr.write('{}'.format(exc))
+        sys.stderr.write('{}\n'.format(exc))
