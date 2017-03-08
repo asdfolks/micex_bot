0fffcd99555b8b947780f4c771a68221c91bbbe7
Aleksey Kutepov <kutepoff@gmail.com>
2017-03-08 13:04:25 +0300

    EUR and GBP support

--- micex_bot.py
+++ micex_bot.py
@@ -19,7 +19,6 @@ _TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
 _API_PREFIX = 'https://api.telegram.org/bot'
 _API = _API_PREFIX + _TOKEN
 _WEB_HOOK = os.environ.get('WEBHOOK_HOSTNAME') + urllib.quote_plus(_TOKEN)
-_MICEX_USDRUB_URL = 'http://www.micex.ru/issrpc/marketdata/currency/selt/daily/preview/result.json?collection_id=173&board_group_id=13'
 _YAHOO_FINANCE_RUBKRW_URL = 'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.xchange%20WHERE%20pair%3D%22RUBKRW%22%20%7C%20truncate(count%3D1)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
 _YAHOO_FINANCE_KRWRUB_URL = 'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.xchange%20WHERE%20pair%3D%22KRWRUB%22%20%7C%20truncate(count%3D1)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
 _YAHOO_FINANCE_USDKRW_URL = 'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.xchange%20WHERE%20pair%3D%22USDKRW%22%20%7C%20truncate(count%3D1)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
@@ -65,32 +64,39 @@ def send_yahoo_finance_krw_data(to, base):
         r.status_code, r.text))
 
 
-def send_micex_usdrub_data(to):
-
-    usa_flag = u'\U0001f1fa\U0001f1f8'
-    rub_sign = u'\u20bd'
-    day_sign = u'\U0001f307'
-    night_sign = u'\U0001f303'
-
-    if to is None:
-        sys.stderr.write('Cannot find chat_id\n')
-    r = requests.get(_MICEX_USDRUB_URL)
+def extract_moex_data(currency):
+    url_map = {
+        'usd': 'http://www.micex.ru/issrpc/marketdata/currency/selt/daily/preview/result.json?collection_id=173&board_group_id=13',
+        'eur': 'http://www.micex.ru/issrpc/marketdata/currency/selt/daily/preview/result.json?collection_id=172&board_group_id=13',
+        'gbp': 'http://www.micex.ru/issrpc/marketdata/currency/selt/daily/preview/result.json?collection_id=171&board_group_id=13',
+    }
+    url = url_map.get(currency.lower())
+    r = requests.get(url)
     sys.stderr.write('MICEX reply {0}: {1}\n'.format(r.status_code, r.text))
-    data = json.loads(r.text.decode('utf-8'))
+    return json.loads(r.text.decode('utf-8'))
+
+
+def transform_moex_data(data):
+    rub_sign = u'\u20bd'
+    supported_currencies = ['USD', 'EUR', 'GBP']
     message = ''
     message_template = u'{FLAG_SIGN}{TOD_TOM_SIGN} {LAST}{MONEY_SIGN} {CHANGE_PCT}%{UP_OR_DOWN_SIGN}\n'
+
     for ticker in data:
         if isinstance(ticker, dict):
 
             name = ticker['SHORTNAME']
-            if not ('_TOD' in name or '_TOM' in name):
-                continue
-
             delta = ticker['CHANGE']
             value = ticker['LAST']
 
-            ticker['TOD_TOM_SIGN'] = day_sign if 'TOD' in name else night_sign
-            ticker['FLAG_SIGN'] = usa_flag
+            is_tod_tom = any(name.endswith(s) for s in ['_TOD', '_TOM'])
+            is_usd_eur_gbp = any(name.startswith(c) for c in supported_currencies)
+
+            if not (is_tod_tom and is_usd_eur_gbp):
+                continue
+
+            ticker['TOD_TOM_SIGN'] = get_tod_tom_sign(name)
+            ticker['FLAG_SIGN'] = get_currency_flag(name)
             ticker['MONEY_SIGN'] = rub_sign
             ticker['UP_OR_DOWN_SIGN'] = get_up_or_down_sign(delta)
             ticker['CHANGE_PCT'] = get_delta_in_percents(value, delta)
@@ -98,6 +104,10 @@ def send_micex_usdrub_data(to):
 
             msg = message_template.format(**ticker)
             message += msg
+    return message
+
+
+def send_moex_data(to, message):
     headers = {'Content-Type': 'application/json'}
     r = requests.post(
         _API + '/sendMessage',
@@ -105,6 +115,17 @@ def send_micex_usdrub_data(to):
         headers=headers)
     sys.stderr.write('Telegram sendMessage reply {0}: {1}'.format(
         r.status_code, r.text))
+    return r
+
+
+def process_micex_currency_data(to, currency):
+
+    if to is None:
+        sys.stderr.write('Cannot find chat_id\n')
+
+    data = extract_moex_data(currency)
+    message = transform_moex_data(data)
+    send_moex_data(to, message)
 
 
 def get_delta_in_percents(value, delta):
@@ -124,6 +145,26 @@ def get_up_or_down_sign(delta):
         return ''
 
 
+def get_currency_flag(ticker_name):
+    usa_flag = u'\U0001f1fa\U0001f1f8'
+    eur_flag = u'\U0001f1ea\U0001f1fa'
+    gbp_flag = u'\U0001f1ec\U0001f1e7'
+
+    if ticker_name.startswith('USD'):
+        return usa_flag
+    elif ticker_name.startswith('EUR'):
+        return eur_flag
+    elif ticker_name.startswith('GBP'):
+        return gbp_flag
+
+
+def get_tod_tom_sign(ticker_name):
+    day_sign = u'\U0001f307'
+    night_sign = u'\U0001f303'
+
+    return day_sign if ticker_name.endswith('_TOD') else night_sign
+
+
 def parse_command(update):
     message = update.get('message', {}).get('text', '')
     match = re.match(
@@ -141,8 +182,8 @@ def web_hook():
 
     command, _ = parse_command(data)
     chat_id = data.get('message', {}).get('chat', {}).get('id')
-    if command == 'usd':
-        send_micex_usdrub_data(chat_id)
+    if command in ['usd', 'eur', 'gbp']:
+        process_micex_currency_data(chat_id, command)
     elif command == 'rubikilowon':
         send_yahoo_finance_krw_data(chat_id, 'kilokrw')
         send_yahoo_finance_krw_data(chat_id, 'usd')
